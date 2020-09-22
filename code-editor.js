52240c300cdd531dc1bf7998bb16e03b8ce94a48
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2020-09-22 12:35:39 -0500

    Update code-editor.js

--- code-editor.js
+++ code-editor.js
@@ -1,17 +1,17 @@
-  $( function() {
-    $( "#editor-dialog" ).dialog({
-      autoOpen: false,
-      show: {
-        effect: "blind",
-        duration: 1000
-      },
-      hide: {
-        effect: "explode",
-        duration: 1000
-      }
-    });
- 
-    $( "#editor-dialog-opener" ).on( "click", function() {
-      $( "#editor-dialog" ).dialog( "open" );
-    });
-  } );
+$( function() {
+  $( "#editor-dialog" ).dialog({
+    autoOpen: false,
+    show: {
+      effect: "blind",
+      duration: 1000
+    },
+    hide: {
+      effect: "explode",
+      duration: 1000
+    }
+  });
+  $( "#editor-dialog-opener" ).on( "click", function() {
+    $( "#editor-dialog" ).dialog( "open" );
+  });
+  $( "#editor-tabs" ).tabs();
+} );

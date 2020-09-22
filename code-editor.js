cfe0a61737d2e8d68054d51715f6d5cbb482d06c
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2020-09-22 12:40:14 -0500

    Add goHtml function

--- code-editor.js
+++ code-editor.js
@@ -15,3 +15,30 @@ $( function() {
   });
   $( "#editor-tabs" ).tabs();
 } );
+
+
+function goHtml() {
+  const html = document.getElementById("htmlInput").value;
+  const css = document.getElementById("cssInput").value;
+  const js = document.getElementById("jsInput").value;
+  const template = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Editor Content</title>
+        <style>
+          ${css}
+        </style>
+      </head>
+      <body>
+        ${html}
+        <script>
+          ${js}
+        <\/script>
+      </body>
+    </html>
+  `;
+  const blob = new Blob([template], {type: 'text/html'});
+  document.getElementById("codeResult").src = URL.createObjectURL(blob);
+}

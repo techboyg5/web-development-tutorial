763600bc5789b0d8644aba7923c880376fec6571
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2020-09-27 15:10:10 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,21 @@
+var CACHE_NAME = 'wdt-cache';
+var urlsToCache = [
+  '/web-development-tutorial/index.html',
+  '/web-development-tutorial/chapter1/page1.html',
+  '/web-development-tutorial/chapter2/page1.html',
+  '/web-development-tutorial/chapter2/page2.html',
+  '/web-development-tutorial/html5.svg',
+  '/web-development-tutorial/manifest.webmanifest',
+  '/web-development-tutorial/script.js',
+  '/web-development-tutorial/style.css'
+];
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});

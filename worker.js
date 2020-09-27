2e060af6f6ad87d1a598f944a33889d3a1a0e82f
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2020-09-27 15:17:18 -0500

    Add external resources

--- worker.js
+++ worker.js
@@ -7,7 +7,14 @@ var urlsToCache = [
   '/web-development-tutorial/html5.svg',
   '/web-development-tutorial/manifest.webmanifest',
   '/web-development-tutorial/script.js',
-  '/web-development-tutorial/style.css'
+  '/web-development-tutorial/style.css',
+  'https://www.googletagmanager.com/gtag/js?id=UA-167358769-2',
+  'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+  'https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery-contextmenu/2.7.1/jquery.contextMenu.min.css',
+  'https://code.jquery.com/jquery-3.5.1.min.js',
+  'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery-contextmenu/2.7.1/jquery.contextMenu.min.js'
 ];
 
 self.addEventListener('install', function(event) {

bfeb51a9a5a5dc09d70092fc782a321cfaf87d4e
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2020-09-27 15:11:12 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -19,3 +19,16 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});

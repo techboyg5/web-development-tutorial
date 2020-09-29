78fc66de3f15aeccac0b5f38c21e82de6b821e3e
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2020-09-29 08:02:38 -0500

    Fix frambuster not working

--- script.js
+++ script.js
@@ -72,14 +72,14 @@ function light() {
   document.documentElement.classList.remove("dark");
 }
 
-if (window.top != window.self) {
-  document.body.innerHTML = `
-    <h2>Framing Not Allowed</h2>
-    <hr>
-    <p>Framing of the techboyg5 Web Development Tutorial is forbidden.</p>
-    <p><a href="" target="_blank">Open this page in a new window.</a></p>
-    <footer class="footer">
-      <p>© 2020 by techboyg5. Licensed under the MIT License.</p>
-	</footer>
-  `;
+if (window.top !== window.self) {
+	document.body.innerHTML = `
+		<h2>Framing Not Allowed</h2>
+		<hr>
+		<p>Framing of the techboyg5 Web Development Tutorial is forbidden.</p>
+		<p><a href="" target="_blank">Open this page in a new window.</a></p>
+		<footer class="footer">
+			<p>© 2020 by techboyg5. Licensed under the MIT License.</p>
+		</footer>
+	`;
 }

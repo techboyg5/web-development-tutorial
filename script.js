70a2eac22cb344fc299846deb56b848a1dc117dd
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2020-10-11 15:16:13 -0500

    Remove Code Editor dialog fratuer

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-$( function() {
-  $( "#editor-dialog" ).dialog({
-    autoOpen: false,
-    width: 800,
-    show: {
-        effect: "scale",
-        duration: 200
-    },
-    hide: {
-        effect: "scale",
-        duration: 200
-    },
-    buttons: {
-      "Close": function() {
-        $( this ).dialog( "close" );
-      }
-    }
-  });
-  $( "#editor-dialog-opener" ).on( "click", function() {
-    $( "#editor-dialog" ).dialog( "open" );
-  });
-} );
-
 var topNavigation = document.getElementsByClassName("tutorial-nav")[0];
 document.addEventListener("scroll", stickNav);
 function stickNav() {

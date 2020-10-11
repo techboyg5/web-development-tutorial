40dd3528b0aca29d4d585a3402faa57cbbf6a31f
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2020-10-11 15:17:25 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-var topNavigation = document.getElementsByClassName("tutorial-nav")[0];
-document.addEventListener("scroll", stickNav);
-function stickNav() {
-  topNavigation.style.top = window.scrollY + 'px';
-}
